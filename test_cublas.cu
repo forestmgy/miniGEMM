7b29078a16fab6426cff967f113005c32996e6be
guangyu <guangyu@chalmers.se>
2026-03-21 21:21:31 +0100

    update

--- test_cublas.cu
+++ test_cublas.cu
@@ -1,21 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <float.h>
+#include <math.h>
+#include <time.h>
 #include <cublas_v2.h>
 #include <cuda_runtime.h>
 
 #define OFFSET(row, col, ld) ((row) * (ld) + (col))
-#define FLOAT4(pointer) (reinterpret_cast<float4*>(&(pointer))[0])
 
-float testCublasError(const int M, const int N, const int K);
-float testCublasPerformance(const int M, const int N, const int K, const int repeat);
+float testCublasErrorAndFlops(const int M, const int N, const int K, const int repeat);
 
-void cpuSgemm(
-    float *a, float *b, float *c, const int M, const int N, const int K) {
-
+void cpuSgemm(float *a, float *b, float *c, const int M, const int N, const int K) {
     for (int m = 0; m < M; m++) {
         for (int n = 0; n < N; n++) {
-            float psum = 0.0;
+            float psum = 0.0f;
             for (int k = 0; k < K; k++) {
                 psum += a[OFFSET(m, k, K)] * b[OFFSET(k, n, N)];
             }
@@ -24,42 +22,40 @@ void cpuSgemm(
     }
 }
 
-
-
 int main(void) {
-    printf("\nKernal = cublas\n");
-    const int outer_repeat = 10, inner_repeat = 1;
-    {
-        const int M = 512, N = 512, K = 512;
-        float max_error = testCublasError(M, N, K);
-        printf("Max Error = %f\n", max_error);
-    }
+    printf("\nKernel = cublas\n");
+
+    const int M = 4096, N = 4096, K = 4096;
+    const int repeat = 100;
+
+    float max_error = testCublasErrorAndFlops(M, N, K, repeat);
+    printf("Max Error = %f\n", max_error);
 
     return 0;
 }
 
+float testCublasErrorAndFlops(const int M, const int N, const int K, const int repeat) {
+    size_t size_a = (size_t)M * K * sizeof(float);
+    size_t size_b = (size_t)K * N * sizeof(float);
+    size_t size_c = (size_t)M * N * sizeof(float);
 
-float testCublasError(const int M, const int N, const int K) {
+    float *h_a, *h_b, *h_c, *h_d_c;
+    float *d_a, *d_b, *d_c;
 
-    size_t size_a = M * K * sizeof(float);
-    size_t size_b = K * N * sizeof(float);
-    size_t size_c = M * N * sizeof(float);
-
-    float *h_a, *h_b, *h_c, *d_a, *d_b, *d_c, *h_d_c;
     h_a = (float *)malloc(size_a);
     h_b = (float *)malloc(size_b);
     h_c = (float *)malloc(size_c);
+    h_d_c = (float *)malloc(size_c);
+
     cudaMalloc(&d_a, size_a);
     cudaMalloc(&d_b, size_b);
     cudaMalloc(&d_c, size_c);
-    h_d_c = (float *)malloc(size_c);
 
-    srand(time(0));
-    for (int i = 0; i < M * K; i++)
-        h_a[i] = rand() / float(RAND_MAX);
-    for (int i = 0; i < K * N; i++)
-        h_b[i] = rand() / float(RAND_MAX);
+    srand((unsigned)time(0));
+    for (int i = 0; i < M * K; i++) h_a[i] = rand() / (float)RAND_MAX;
+    for (int i = 0; i < K * N; i++) h_b[i] = rand() / (float)RAND_MAX;
 
+    // CPU reference
     cpuSgemm(h_a, h_b, h_c, M, N, K);
 
     cudaMemcpy(d_a, h_a, size_a, cudaMemcpyHostToDevice);
@@ -67,32 +63,85 @@ float testCublasError(const int M, const int N, const int K) {
 
     cublasHandle_t cublas_handle;
     cublasCreate(&cublas_handle);
-    float cublas_alpha = 1.0;
-    float cublas_beta = 0;
-    // cublasSgemm(cublas_handle, CUBLAS_OP_T, CUBLAS_OP_T, M, N, K, &cublas_alpha, d_a, K, d_b, N, &cublas_beta, d_c, M);
-    cublasSgemm(cublas_handle, CUBLAS_OP_N, CUBLAS_OP_N, N, M, K, &cublas_alpha, d_b, N, d_a, K, &cublas_beta, d_c, N);
 
+    float alpha = 1.0f;
+    float beta = 0.0f;
+
+    // warmup
+    cublasSgemm(
+        cublas_handle,
+        CUBLAS_OP_N, CUBLAS_OP_N,
+        N, M, K,
+        &alpha,
+        d_b, N,
+        d_a, K,
+        &beta,
+        d_c, N
+    );
+    cudaDeviceSynchronize();
+
+    // timing
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    cudaEventRecord(start);
+    for (int i = 0; i < repeat; i++) {
+        cublasSgemm(
+            cublas_handle,
+            CUBLAS_OP_N, CUBLAS_OP_N,
+            N, M, K,
+            &alpha,
+            d_b, N,
+            d_a, K,
+            &beta,
+            d_c, N
+        );
+    }
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+
+    float total_ms = 0.0f;
+    cudaEventElapsedTime(&total_ms, start, stop);
+    float avg_ms = total_ms / repeat;
+
+    // copy result back
     cudaMemcpy(h_d_c, d_c, size_c, cudaMemcpyDeviceToHost);
 
-    float max_error = 0.0;
+    // check error
+    float max_error = 0.0f;
     for (int i = 0; i < M * N; i++) {
-        float this_error = abs(h_d_c[i] - h_c[i]);
-        if (max_error != max_error || this_error != this_error) // nan
-            max_error = -NAN;
-        else
-            max_error = max(max_error, this_error);
+        float this_error = fabsf(h_d_c[i] - h_c[i]);
+        if (isnan(this_error)) {
+            max_error = NAN;
+            break;
+        }
+        if (this_error > max_error) {
+            max_error = this_error;
+        }
     }
+
+    // FLOPs = 2 * M * N * K
+    double total_flops = 2.0 * (double)M * (double)N * (double)K;
+    double gflops = total_flops / (avg_ms / 1000.0) / 1e9;
+    double tflops = gflops / 1000.0;
+
+    printf("M = %d, N = %d, K = %d\n", M, N, K);
+    printf("Total FLOPs per GEMM = %.0f\n", total_flops);
+    printf("Average Time = %.6f ms\n", avg_ms);
+    printf("Performance = %.3f GFLOPS (%.6f TFLOPS)\n", gflops, tflops);
+
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+    cublasDestroy(cublas_handle);
 
     free(h_a);
     free(h_b);
     free(h_c);
+    free(h_d_c);
     cudaFree(d_a);
     cudaFree(d_b);
     cudaFree(d_c);
-    free(h_d_c);
 
     return max_error;
 }
-
-
-
--- test_v3.cu
+++ test_v3.cu
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <float.h>
+#include <math.h>
+#include <time.h>
 #include <cuda_runtime.h>
 
 #define OFFSET(row, col, ld) ((row) * (ld) + (col))
 #define FLOAT4(pointer) (reinterpret_cast<float4*>(&(pointer))[0])
 
 float testError(
-    void (*gpuSgemm) (float *, float *, float *, const int, const int, const int),
+    void (*gpuSgemm)(float *, float *, float *, const int, const int, const int),
     dim3 gridDim, dim3 blockDim, const int M, const int N, const int K);
-float testPerformance(
-    void (*gpuSgemm) (float *, float *, float *, const int, const int, const int),
+
+void testPerformance(
+    void (*gpuSgemm)(float *, float *, float *, const int, const int, const int),
     dim3 gridDim, dim3 blockDim, const int M, const int N, const int K, const int repeat);
 
-void cpuSgemm(
-    float *a, float *b, float *c, const int M, const int N, const int K) {
-
+void cpuSgemm(float *a, float *b, float *c, const int M, const int N, const int K) {
     for (int m = 0; m < M; m++) {
         for (int n = 0; n < N; n++) {
-            float psum = 0.0;
+            float psum = 0.0f;
             for (int k = 0; k < K; k++) {
                 psum += a[OFFSET(m, k, K)] * b[OFFSET(k, n, N)];
             }
@@ -26,7 +27,6 @@ void cpuSgemm(
         }
     }
 }
-
 
 __global__ void sgemm_V3(
     float * __restrict__ a, float * __restrict__ b, float * __restrict__ c,
@@ -51,7 +51,7 @@ __global__ void sgemm_V3(
     float r_load_b[4];
     float r_comp_a[TM];
     float r_comp_b[TN];
-    float r_c[TM][TN] = {0.0};
+    float r_c[TM][TN] = {0.0f};
 
     int load_a_smem_m = tid >> 1;
     int load_a_smem_k = (tid & 1) << 2;
@@ -77,7 +77,6 @@ __global__ void sgemm_V3(
     }
 
     for (int bk = 1; bk < (K + BK - 1) / BK; bk++) {
-
         int smem_sel = (bk - 1) & 1;
         int smem_sel_next = bk & 1;
 
@@ -147,74 +146,144 @@ __global__ void sgemm_V3(
     }
 }
 
-
-
 int main(void) {
-    printf("\nKernal = sgemm_V3\n");
-    const int outer_repeat = 10, inner_repeat = 1;
-    const int BM = 128, BN = 128, TM = 8, TN = 8;
-    void (*gpuSgemm) (float *, float *, float *, const int, const int, const int) = sgemm_V3;
+    printf("\nKernel = sgemm_V3\n");
 
-    {
-        const int M = 512, N = 512, K = 512;
-        dim3 blockDim(BN / TN, BM / TM);
-        dim3 gridDim((N + BN - 1) / BN, (M + BM - 1) / BM);
-        float max_error = testError(gpuSgemm, gridDim, blockDim, M, N, K);
-        printf("Max Error = %f\n", max_error);
-    }
+    const int BM = 128, BN = 128, TM = 8, TN = 8;
+    const int M = 4096, N = 4096, K = 4096;
+    const int repeat = 100;
+
+    void (*gpuSgemm)(float *, float *, float *, const int, const int, const int) = sgemm_V3;
+
+    dim3 blockDim(BN / TN, BM / TM);
+    dim3 gridDim((N + BN - 1) / BN, (M + BM - 1) / BM);
+
+    float max_error = testError(gpuSgemm, gridDim, blockDim, M, N, K);
+    printf("Max Error = %f\n", max_error);
+
+    testPerformance(gpuSgemm, gridDim, blockDim, M, N, K, repeat);
 
     return 0;
 }
 
-
 float testError(
-    void (*gpuSgemm) (float *, float *, float *, const int, const int, const int),
+    void (*gpuSgemm)(float *, float *, float *, const int, const int, const int),
     dim3 gridDim, dim3 blockDim, const int M, const int N, const int K) {
 
-    size_t size_a = M * K * sizeof(float);
-    size_t size_b = K * N * sizeof(float);
-    size_t size_c = M * N * sizeof(float);
+    size_t size_a = (size_t)M * K * sizeof(float);
+    size_t size_b = (size_t)K * N * sizeof(float);
+    size_t size_c = (size_t)M * N * sizeof(float);
 
-    float *h_a, *h_b, *h_c, *d_a, *d_b, *d_c, *h_d_c;
+    float *h_a, *h_b, *h_c, *h_d_c;
+    float *d_a, *d_b, *d_c;
+
     h_a = (float *)malloc(size_a);
     h_b = (float *)malloc(size_b);
     h_c = (float *)malloc(size_c);
+    h_d_c = (float *)malloc(size_c);
+
     cudaMalloc(&d_a, size_a);
     cudaMalloc(&d_b, size_b);
     cudaMalloc(&d_c, size_c);
-    h_d_c = (float *)malloc(size_c);
 
-    srand(time(0));
-    for (int i = 0; i < M * K; i++)
-        h_a[i] = rand() / float(RAND_MAX);
-    for (int i = 0; i < K * N; i++)
-        h_b[i] = rand() / float(RAND_MAX);
+    srand((unsigned)time(0));
+    for (int i = 0; i < M * K; i++) h_a[i] = rand() / (float)RAND_MAX;
+    for (int i = 0; i < K * N; i++) h_b[i] = rand() / (float)RAND_MAX;
     cudaMemset(d_c, 15, size_c);
 
     cpuSgemm(h_a, h_b, h_c, M, N, K);
 
     cudaMemcpy(d_a, h_a, size_a, cudaMemcpyHostToDevice);
     cudaMemcpy(d_b, h_b, size_b, cudaMemcpyHostToDevice);
+
     gpuSgemm<<<gridDim, blockDim>>>(d_a, d_b, d_c, M, N, K);
+    cudaDeviceSynchronize();
+
     cudaMemcpy(h_d_c, d_c, size_c, cudaMemcpyDeviceToHost);
 
-    float max_error = 0.0;
+    float max_error = 0.0f;
     for (int i = 0; i < M * N; i++) {
-        float this_error = abs(h_d_c[i] - h_c[i]);
-        if (max_error != max_error || this_error != this_error) // nan
-            max_error = -NAN;
-        else
-            max_error = max(max_error, this_error);
+        float this_error = fabsf(h_d_c[i] - h_c[i]);
+        if (isnan(this_error)) {
+            max_error = NAN;
+            break;
+        }
+        if (this_error > max_error) {
+            max_error = this_error;
+        }
     }
 
     free(h_a);
     free(h_b);
     free(h_c);
+    free(h_d_c);
     cudaFree(d_a);
     cudaFree(d_b);
     cudaFree(d_c);
-    free(h_d_c);
 
     return max_error;
 }
 
+void testPerformance(
+    void (*gpuSgemm)(float *, float *, float *, const int, const int, const int),
+    dim3 gridDim, dim3 blockDim, const int M, const int N, const int K, const int repeat) {
+
+    size_t size_a = (size_t)M * K * sizeof(float);
+    size_t size_b = (size_t)K * N * sizeof(float);
+    size_t size_c = (size_t)M * N * sizeof(float);
+
+    float *h_a, *h_b;
+    float *d_a, *d_b, *d_c;
+
+    h_a = (float *)malloc(size_a);
+    h_b = (float *)malloc(size_b);
+
+    cudaMalloc(&d_a, size_a);
+    cudaMalloc(&d_b, size_b);
+    cudaMalloc(&d_c, size_c);
+
+    srand(123);
+    for (int i = 0; i < M * K; i++) h_a[i] = rand() / (float)RAND_MAX;
+    for (int i = 0; i < K * N; i++) h_b[i] = rand() / (float)RAND_MAX;
+
+    cudaMemcpy(d_a, h_a, size_a, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_b, size_b, cudaMemcpyHostToDevice);
+    cudaMemset(d_c, 0, size_c);
+
+    // warmup
+    gpuSgemm<<<gridDim, blockDim>>>(d_a, d_b, d_c, M, N, K);
+    cudaDeviceSynchronize();
+
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    cudaEventRecord(start);
+    for (int i = 0; i < repeat; i++) {
+        gpuSgemm<<<gridDim, blockDim>>>(d_a, d_b, d_c, M, N, K);
+    }
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+
+    float total_ms = 0.0f;
+    cudaEventElapsedTime(&total_ms, start, stop);
+    float avg_ms = total_ms / repeat;
+
+    double total_flops = 2.0 * (double)M * (double)N * (double)K;
+    double gflops = total_flops / (avg_ms / 1000.0) / 1e9;
+    double tflops = gflops / 1000.0;
+
+    printf("M = %d, N = %d, K = %d\n", M, N, K);
+    printf("Total FLOPs per GEMM = %.0f\n", total_flops);
+    printf("Average Time = %.6f ms\n", avg_ms);
+    printf("Performance = %.3f GFLOPS (%.6f TFLOPS)\n", gflops, tflops);
+
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+
+    free(h_a);
+    free(h_b);
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+}
